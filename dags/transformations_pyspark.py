6850a515459f77125de87f20351784308f72ea6c
Daniel Gomez Perry <danielrgomez@gmail.com>
2025-03-12 19:14:56 -0400

    modified:   dags/transformations_pyspark.py

--- dags/transformations_pyspark.py
+++ dags/transformations_pyspark.py
@@ -2,6 +2,7 @@ import pyspark
 from pyspark.sql import SparkSession
 from pyspark.sql.functions import when, col
 from pyspark.sql.functions import to_timestamp, to_date
+import json
 
 #spark = SparkSession.builder.appName("Transformations_NYC_Fire_Incidents").getOrCreate()
 #print(spark.version)
@@ -15,7 +16,19 @@ def pyspark_transformations(json_results):
         .config("spark.driver.maxResultSize", "2g") \
         .getOrCreate()
 
-    #json_results_as_a_string = str(json_results)
-    #df = spark.read.json(spark.sparkContext.parallelize([json_results]))
+    # Validate JSON format
+    try:
+        json.loads(json_results)
+        print("Valid JSON")
+    except json.JSONDecodeError as e:
+        print(f"Invalid JSON: {e}")
+
+    json_results = f"[{json_results}]"
+        
+    json_list = json.loads(json_results)
+        
+    df = spark.read.json(spark.sparkContext.parallelize([json_list]))
+    df.show()
+    #df = spark.read.json(json_results)
     print("hello world")
     
