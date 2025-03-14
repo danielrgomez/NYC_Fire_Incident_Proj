3a2b2018452e0d4ca714f7edd70362609a7a0507
Daniel Gomez Perry <danielrgomez@gmail.com>
2025-03-13 22:58:23 -0400

    modified:   dags/transformations_pyspark.py 	modified:   pull_fire_incidents.ipynb

--- dags/transformations_pyspark.py
+++ dags/transformations_pyspark.py
@@ -3,6 +3,7 @@ from pyspark.sql import SparkSession
 from pyspark.sql.functions import when, col
 from pyspark.sql.functions import to_timestamp, to_date
 import json
+import pandas as pd
 
 #spark = SparkSession.builder.appName("Transformations_NYC_Fire_Incidents").getOrCreate()
 #print(spark.version)
@@ -28,7 +29,7 @@ def pyspark_transformations(json_results):
         .appName("FireIncidents") \
         .config("spark.driver.memory", "4g") \
         .config("spark.executor.memory", "4g") \
-        .config("spark.driver.maxResultSize", "2g") \
+        .config("spark.driver.maxResultSize", "4g") \
         .getOrCreate()
 
     # Validate JSON format
@@ -38,9 +39,22 @@ def pyspark_transformations(json_results):
     except json.JSONDecodeError as e:
         print(f"Invalid JSON: {e}")
 
+    # Parse the JSON and flatten it
+    parsed_json = json.loads(json_results)
+    df = pd.DataFrame(parsed_json)
+    print("Parse the JSON and flatten it")
+    # Convert the DataFrame to a list of dictionaries
+    
+    flat_json = df.to_dict(orient="records")
+    print("Set json to dictionary")
+
+    df = spark.read.json(spark.sparkContext.parallelize(flat_json))
+    df.show()
+
     #json_results = f"[{json_results}]"
-    print(type(json_results))
-    print(json_results)
+    #print("Json Results Type: !!")
+    #print(type(json_results))
+    #print(json_results)
     #json_results = json_results.replace("'", '"')
     
     #json_list = json.loads(json_results)
@@ -55,23 +69,46 @@ def pyspark_transformations(json_results):
     #print("Printing JSON LIST")     
     #print(json_list)
     #df = spark.read.json(json_results)
+
+    ## Validate JSON format
+    #try:
+    #    json.loads(json_list)
+    #    print("json_list is Valid JSON")
+    #except json.JSONDecodeError as e:
+    #    print(f"json_list is Invalid JSON: {e}")
+
+    # Convert single JSON object into a list
+    #json_results_list = [json_results]
     
-    #df = spark.read.json(spark.sparkContext.parallelize([json_results]))
-    #df.show()
+    #processed_json = json.dumps(json_results)
+    #print("Processed JSON Type:")
+    #print(type(processed_json))
+
+    
+
+#    print("Show Corrupt Record: ")
+#    # Read the JSON file
+#    df_corrupt = spark.read.option("mode", "PERMISSIVE").option("columnNameOfCorruptRecord", "_corrupt_record").json(spark.sparkContext.parallelize([json_list]))
+#
+#    # Filter for corrupt records
+#    corrupt_records = df_corrupt.filter(df_corrupt["_corrupt_record"].isNotNull())
+#
+#    # Show the corrupt records
+#    corrupt_records.show(truncate=False)
 #
 #    
 #    
 #    #The values presented below correspond to the first entry identified for each field within their respective boroughs. For instance, in the case of the Bronx, the first zip code encountered in the dataset was 10451.  
 #    #For the null values, it is assumed that the newly assigned values will approximate the actual values as closely as possible.
 #    df = clean_null_values(df,"zipcode",10451,11201,10001,11004,10301)
-#    df = clean_null_values(df,"policeprecinct",40,60,1,100,120)
+    df = clean_null_values(df,"policeprecinct",40,60,1,100,120)
 #    df = clean_null_values(df,"citycouncildistrict",8,33,1,19,49)
 #    df = clean_null_values(df,"communitydistrict",201,301,101,401,501)
 #    df = clean_null_values(df,"communityschooldistrict",7,13,1,7,31)
 #    df = clean_null_values(df,"congressionaldistrict",13,7,7,3,11)
 #
-#    print("Number of pliiceprecinct null values: ")
-#    df.where(df["policeprecinct"].isNull()).select("starfire_incident_id","zipcode","alarm_box_borough").count()
+    print("Number of pliiceprecinct null values: ")
+    print(df.where(df["policeprecinct"].isNull()).select("starfire_incident_id","zipcode","alarm_box_borough").count())
 
     #df = spark.read.json(json_results)
     print("hello world")
